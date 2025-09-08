9f5c892861122da147fb9827efce2ed9cd5bea7b
rdcolwell <72354938+rdcolwell@users.noreply.github.com>
2025-09-08 15:56:44 +0000

    Deploying to gh-pages from @ rdcolwell/rdcolwell.github.io@1fb0b3e03ecff52b6a5335972665a29437b89db7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -564,7 +564,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=hTGVpOUAAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
